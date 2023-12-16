bd42454cfaaad4dc4fa270c1b18a3e9fe527cb5b
Mafalda Sousa <180323044@estudantes.ips.pt>
2023-12-16 15:20:12 +0000

    Mafalda commiting changes to MySQL part of Lab | SQL Queries 9

--- Lab_SQL Queries 9.sql
+++ Lab_SQL Queries 9.sql
@@ -25,7 +25,7 @@ select * from (select * from sakila.rental
 
 select * from sakila.rentals_may;
 
--- Create a table rentals_june to store the data from rental table with information for the month of June.
+-- -> Create a table rentals_june to store the data from rental table with information for the month of June.
 drop table if exists rentals_june;
 
 create table `rentals_june` as
@@ -34,7 +34,7 @@ where 1=0;
 
 select * from sakila.rentals_june;
 
--- Insert values in the table rentals_june using the table rental, filtering values only for the month of June.
+-- -> Insert values in the table rentals_june using the table rental, filtering values only for the month of June.
 select * from sakila.rental
 where month(rental_date) = 6;
 
@@ -44,7 +44,7 @@ select * from (select * from sakila.rental
 
 select * from sakila.rentals_june;
 
--- Check the number of rentals for each customer for May.
+-- -> Check the number of rentals for each customer for May.
 select customer_id, count(rental_id) as 'number_of_rentals'
 from sakila.rentals_may
 group by customer_id
@@ -56,7 +56,7 @@ join sakila.rentals_may r using(customer_id)
 group by c.customer_id, c.first_name, c.last_name
 order by count(rental_id) desc;
 
--- Check the number of rentals for each customer for June.
+-- -> Check the number of rentals for each customer for June.
 select customer_id, count(rental_id) as 'Number of rentals'
 from sakila.rentals_june
 group by customer_id
@@ -68,4 +68,27 @@ join sakila.rentals_june r using(customer_id)
 group by c.customer_id, c.first_name, c.last_name
 order by count(rental_id) desc;
 
+/*Write a function that checks if customer borrowed more or less films in the month of June as compared to May. 
+BEFORE trying this with python, we'll see on SQL how to output the result*/
+-- Checking if customer borrowed more or less films in the month of June as compared to May.
+select c.customer_id, concat(c.first_name, ' ', c.last_name) as 'customer_name', 
+	rj.number_of_rentals as 'number_of_may_rentals', rm.number_of_rentals as 'number_of_may_rentals',
+    case 
+		when rj.number_of_rentals > rm.number_of_rentals then 'more'
+        when rj.number_of_rentals < rm.number_of_rentals then 'less'
+        else 'equal'
+	end as 'Compare'
+from sakila.customer c
+join (select c.customer_id, count(r.rental_id) as 'number_of_rentals'
+		from sakila.customer c
+		join sakila.rentals_june r using(customer_id)
+		group by c.customer_id
+		order by count(rental_id) desc
+	) rj using(customer_id)
+join (select c.customer_id, count(r.rental_id) as 'number_of_rentals'
+		from sakila.customer c
+		join sakila.rentals_may r using(customer_id)
+		group by c.customer_id
+		order by count(rental_id) desc
+	) rm using(customer_id);
 
